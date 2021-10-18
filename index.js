d0849914dc4d483af778813b71c7954a81a6ae7f
Edy Tanto <edytanto.chai@gmail.com>
2021-10-18 20:02:10 +0700

    add detail

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const config = {
   headers: {
     'Accept': 'application/vnd.api+json',
     'Content-Type': 'application/vnd.api+json',
-    'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9kdW1teS5hcGkucHNlLnR1cmJpbi5pZFwvYXBpXC92MVwvanNvbmFwaVwvbG9naW4iLCJpYXQiOjE2MzQxOTUyMjIsImV4cCI6MTYzNDQxMTIyMiwibmJmIjoxNjM0MTk1MjIyLCJqdGkiOiJMeE4wakNDdjB5Y1Y5eXdpIiwic3ViIjoxLCJwcnYiOiIyM2JkNWM4OTQ5ZjYwMGFkYjM5ZTcwMWM0MDA4NzJkYjdhNTk3NmY3In0.PEE8Ztu2JIB5013n98FBU4viRrRi2EspJgJP9QBU-BQ'
+    'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9kdW1teS5hcGkucHNlLnR1cmJpbi5pZFwvYXBpXC92MVwvanNvbmFwaVwvbG9naW4iLCJpYXQiOjE2MzQ1NjE2NjgsImV4cCI6MTYzNDc3NzY2OCwibmJmIjoxNjM0NTYxNjY4LCJqdGkiOiJPb3lWbWo2N3BnaklmaDd1Iiwic3ViIjoxLCJwcnYiOiIyM2JkNWM4OTQ5ZjYwMGFkYjM5ZTcwMWM0MDA4NzJkYjdhNTk3NmY3In0.YHiWTqwhj6ngQfp5cFi_PH7nX0Kl9jWco5k0_Ur-UMY'
   }
 };
 
@@ -21,6 +21,14 @@ axios(config)
         const sistemElektronik = daftarSistemElektronik[0];
 
         console.log(sistemElektronik.status.status);
+
+        const daftarPenyelenggaraSistemElektronik = models.findAll('penyelenggaraSistemElektronik');
+        daftarPenyelenggaraSistemElektronik.map((penyelenggaraSistemElektronik) => {
+            const daftarSistemElektronik = penyelenggaraSistemElektronik.sistemElektronik;
+            daftarSistemElektronik.map((sistemElektronik) => {
+                console.log(penyelenggaraSistemElektronik.nama_bh + ' - ' + sistemElektronik.nama_sistem_elektronik);
+            });
+        });
     })
     .catch(function (error) {
         console.log(error);
